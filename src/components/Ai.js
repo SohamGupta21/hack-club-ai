cc2f43c164c65522c408c5860ef927d5e7e2dc68
soham gupta <guptasoham@outlook.com>
2021-04-02 23:33:34 -0500

    first implementation of prediction

--- src/components/Ai.js
+++ src/components/Ai.js
@@ -6,12 +6,23 @@ export default function Ai(){
   const [model, setModel] = useState(null);
   const loadmodel = async (p) => {
     console.log("here")
-    this.model = await tf.loadLayersModel("/tfjs_model/model.json");
+    this.model = await tf.loadLayersModel("https://localhost/tfjs_model/model.json");
     console.log(this.model)
   }
 
   loadmodel();
+
+  const im = new Image();
+  im.src = "five.jpg";
+  im.onload = () => {
+  const a = tf.browser.fromPixels(im, 4);
+  a.print();
+  console.log(a.shape)};
   return (
-    <h1>Loaded</h1>
+    <div>
+      <h1>Loaded</h1>
+      {/* <p>{a.shape}</p> */}
+    </div>
+    
   );
 }
